889d8cc509999b599c79e33d91a3c2e08d340f35
opdushen <kksu0929@gmail.com>
2024-05-30 14:46:37 +0800

    Add files via upload

---
+++ hanoi.cpp
@@ -0,0 +1,28 @@
+﻿#include <iostream>
+#include <iomanip>
+
+using namespace std;
+
+void hanoi(int n, char A, char B, char C);
+
+int main()
+{
+    int n;
+    cout << "Input the num.of Disks:";
+    cin >> n;
+    hanoi(n, 'A', 'B', 'C');
+    return 0;
+}
+
+void hanoi(int n, char X, char Y, char Z)
+{
+    if (n == 1)
+    {
+        cout << n << ":" << X << "-->" << Z << endl;
+        return;
+    }
+    hanoi(n - 1, X, Z, Y);
+    cout << n << ":" << X << "-->" << Z << endl;
+    hanoi(n - 1, Y, X, Z);
+    return;
+}
